569e21f757184883ad0d14beea9423fe6751ad11
Joshua Chan <chan.joshua.j@gmail.com>
2018-07-02 01:42:18 -0400

    Initial commit

---
+++ NewApplication2/app/src/main/java/disciple/dragon/com/newapplication2/Stats.java
@@ -0,0 +1,16 @@
+package disciple.dragon.com.newapplication2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+public class Stats extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.stats_activity);
+
+
+    }
+}
